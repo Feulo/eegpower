400fa019e204ecdc8e07d43e80d19cb8e8e79b51
Feulo <guifeulo@gmail.com>
2015-12-04 18:05:15 -0200

    primeira versão com parser de raw data

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ APP=eegpower
 CC=gcc
 CFLAGS=-g
 LIBS=-lbluetooth
-SRC= main.c
-OBJ=main.o
+SRC= main.c ThinkGearStreamParser.c
+OBJ=main.o ThinkGearStreamParser.o
 
 %.o: %.c
 	$(CC) -c $< -o $@
--- main.c
+++ main.c
@@ -11,14 +11,73 @@
  #include <bluetooth/bluetooth.h>
  #include <bluetooth/rfcomm.h>
 
+//parcer fornecido pela NeuroSky
+#include "ThinkGearStreamParser.h"
+
 /*
  *
  */
+#define PIN 1234
+//aqui que a magia acontece...
+void
+handleDataValueFunc( unsigned char extendedCodeLevel,
+                     unsigned char code,
+                     unsigned char valueLength,
+                     const unsigned char *value,
+                     void *customData ) {
+
+    if( extendedCodeLevel == 0 ) {
+
+        switch( code ) {
+
+            /* [CODE]: ATTENTION eSense */
+            case( 0x04 ):
+                printf( "Attention Level: %d\n", value[0] & 0xFF );
+                break;
+
+            /* [CODE]: MEDITATION eSense */
+            case( 0x05 ):
+                printf( "Meditation Level: %d\n", value[0] & 0xFF );
+                break;
+             //xo colocar o detector de raw data aki pra gente
+                case( 0x80 ):
+                    printf( "RAW DATA: %d\n", (value[0]<<8) | value[1] );
+                    int rawData = (value[0]<<8) | value[1] ;
+                    FILE *fp;
+fp=fopen("test.raw", "a+");
+fputc(rawData, fp);
+fclose(fp);
+                    break;
+              //agora a informação de qualidade de sinal
+              case( 0x02 ):
+                  printf( "POOR SIGNAL: %d\n", value[0] & 0xFF );
+                  break;
+
+              //nivel da bateria
+              case( 0x01 ):
+                  printf( "Battery: %d\n", value[0] & 0xFF );
+                  break;
+            /* Other [CODE]s */
+            default:
+                printf( "EXCODE level: %d CODE: 0x%02X vLength: %d\n",
+                        extendedCodeLevel, code, valueLength );
+                printf( "Data value(s):" );
+                int i = 0;
+                for(i=0; i<valueLength; i++ ) printf( " %02X", value[i] & 0xFF );
+                printf( "\n" );
+        }
+    }
+}
+
+
+
 int main(int argc, char** argv) {
   struct sockaddr_rc addr = { 0 };
-  char buf[1024] = { 0 };
+  //buffer... definir um tamanho adequado
+unsigned char buf = 0 ;
     int s, status, bytes_read;
-    char dest[18] = "00:11:12:07:05:78";
+    //endereço mac do dispositivo bt
+    char dest[18] = "10:14:07:01:11:67";
 
     // allocate a socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
@@ -35,14 +94,32 @@ int main(int argc, char** argv) {
   //  if( status == 0 ) {
   //      status = write(s, "hello!", 6);
   //  }
+//verifica se dispositivo está acessivel
+    if( status != 0 ) perror("Deu Ruim!");
+     else
+     {
+const char command[2] = {0x00};
+      write(s, &command, 1);
 
-    if( status < 0 ) perror("Deu Ruim!");
+
+       ThinkGearStreamParser parser;
+     THINKGEAR_initParser( &parser, PARSER_TYPE_PACKETS,
+                          handleDataValueFunc, NULL );
+
+         while( bytes_read = read(s, &buf, sizeof(buf)) ) {
 
 
-bytes_read = read(s, buf, sizeof(buf));
-  while( bytes_read > 0 ) {
-      printf("received [%s]\n", buf);
-  }
+               THINKGEAR_parseByte( &parser, buf);
+
+
+         }
+
+
+
+
+
+     }
+
 
     close(s);
     return 0;
