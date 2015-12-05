45438703907963bc92d1e3ed7c31ac75920a6503
Feulo <guifeulo@gmail.com>
2015-12-04 23:43:05 -0200

    scan device and select implemented

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  * File:   main.c
  * Author: leandropessoa
- *
+ * Contributor: Feulo
  * Created on 2 de Dezembro de 2015, 01:13
  */
 
@@ -10,117 +10,147 @@
  #include <sys/socket.h>
  #include <bluetooth/bluetooth.h>
  #include <bluetooth/rfcomm.h>
+ #include "ThinkGearStreamParser.h"	//parser fornecido pela NeuroSky
 
-//parcer fornecido pela NeuroSky
-#include "ThinkGearStreamParser.h"
+ #include <bluetooth/hci.h>
+ #include <bluetooth/hci_lib.h>
 
-/*
- *
- */
-#define PIN 1234
+
+
+
+
+ #define PIN 1234
 //aqui que a magia acontece...
-void
-handleDataValueFunc( unsigned char extendedCodeLevel,
-                     unsigned char code,
-                     unsigned char valueLength,
-                     const unsigned char *value,
-                     void *customData ) {
+void handleDataValueFunc( unsigned char extendedCodeLevel,
+	       		  unsigned char code,
+			  unsigned char valueLength,
+			  const unsigned char *value,
+			  void *customData ) {
+	int rawData,i;
+	FILE *fp;
 
-    if( extendedCodeLevel == 0 ) {
+    	if( extendedCodeLevel == 0 ) {
 
-        switch( code ) {
+        	switch( code ) {
+                       	
+			case( 0x04 ):	 /* [CODE]: ATTENTION eSense */
+                		printf( "Attention Level: %d\n", value[0] & 0xFF );
+                	break;
 
-            /* [CODE]: ATTENTION eSense */
-            case( 0x04 ):
-                printf( "Attention Level: %d\n", value[0] & 0xFF );
-                break;
+                       	case( 0x05 ): 	/* [CODE]: MEDITATION eSense */
+                		printf( "Meditation Level: %d\n", value[0] & 0xFF );
+                	break;
+            
+       			case( 0x80 ):	//xo colocar o detector de raw data aki pra gente
+                    		printf( "RAW DATA: %d\n", (value[0]<<8) | value[1] );
+                     		rawData = (value[0]<<8) | value[1] ;    
+				fp=fopen("test.raw", "a+");
+				fputc(rawData, fp);
+				fclose(fp);
+                    	break;
+                          
+			case( 0x02 ): //agora a informação de qualidade de sinal
+                  		printf( "POOR SIGNAL: %d\n", value[0] & 0xFF );
+                  	break;
 
-            /* [CODE]: MEDITATION eSense */
-            case( 0x05 ):
-                printf( "Meditation Level: %d\n", value[0] & 0xFF );
-                break;
-             //xo colocar o detector de raw data aki pra gente
-                case( 0x80 ):
-                    printf( "RAW DATA: %d\n", (value[0]<<8) | value[1] );
-                    int rawData = (value[0]<<8) | value[1] ;
-                    FILE *fp;
-fp=fopen("test.raw", "a+");
-fputc(rawData, fp);
-fclose(fp);
-                    break;
-              //agora a informação de qualidade de sinal
-              case( 0x02 ):
-                  printf( "POOR SIGNAL: %d\n", value[0] & 0xFF );
-                  break;
-
-              //nivel da bateria
-              case( 0x01 ):
-                  printf( "Battery: %d\n", value[0] & 0xFF );
-                  break;
-            /* Other [CODE]s */
-            default:
-                printf( "EXCODE level: %d CODE: 0x%02X vLength: %d\n",
-                        extendedCodeLevel, code, valueLength );
-                printf( "Data value(s):" );
-                int i = 0;
-                for(i=0; i<valueLength; i++ ) printf( " %02X", value[i] & 0xFF );
-                printf( "\n" );
-        }
-    }
+                       	case( 0x01 ): 
+                  		printf( "Battery: %d\n", value[0] & 0xFF );
+                  	break;
+            
+            		default:	/* Other [CODE]s */
+                		printf( "EXCODE level: %d CODE: 0x%02X vLength: %d\n", extendedCodeLevel, code, valueLength );
+                		printf( "Data value(s):" );
+                		i = 0;
+                		for(i=0; i<valueLength; i++ ) printf( " %02X", value[i] & 0xFF );
+                		printf( "\n" );
+        	}
+    	}
 }
 
 
 
 int main(int argc, char** argv) {
-  struct sockaddr_rc addr = { 0 };
-  //buffer... definir um tamanho adequado
-unsigned char buf = 0 ;
-    int s, status, bytes_read;
-    //endereço mac do dispositivo bt
-    char dest[18] = "10:14:07:01:11:67";
+
+	struct sockaddr_rc addr = { 0 };	 //buffer... definir um tamanho adequado
+	int s, status, bytes_read;		 
+	char dest[18];// = "10:14:07:01:11:67";	//endereço mac do dispositivo bt
 	const char command[2] = {0x00};
-    // allocate a socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+	int dev_id,sock,len,flags;
+	int max_rsp, num_rsp;
+	int i,pos;
+	inquiry_info *ii = NULL;
+	char name[248] = { 0 };
+	char addr_1[19] = { 0 };
+	unsigned char buf = 0 ;
+	char **addrs;	
+	
+	dev_id = hci_get_route(NULL);          //comunicacao com placa bluetooth
+    	sock = hci_open_dev( dev_id );
+	if (dev_id < 0 || sock < 0) {
+		perror("opening socket");
+	        exit(1);
+	}
 
-    // set the connection parameters (who to connect to)
-    addr.rc_family = AF_BLUETOOTH;
-    addr.rc_channel = (uint8_t) 1;
-    str2ba( dest, &addr.rc_bdaddr );
+        len  = 8;
+	max_rsp = 255;
+	flags = IREQ_CACHE_FLUSH;
+	ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
 
-    // connect to server
-    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+	num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+	if( num_rsp < 0 ) perror("hci_inquiry");
+	
+	addrs = (char**)malloc(num_rsp*sizeof(char*));
 
-    // send a message
-  //  if( status == 0 ) {
-  //      status = write(s, "hello!", 6);
-  //  }
-//verifica se dispositivo está acessivel
-    if( status != 0 ) perror("eu Ruim");
-     else
-     {
+	for (i = 0; i < num_rsp; i++) {
+		ba2str(&(ii+i)->bdaddr, addr_1);
+		memset(name, 0, sizeof(name));
+		if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), name, 0) < 0)
+		        strcpy(name, "[unknown]");
+		addrs[i] = (char*)malloc(19*sizeof(char));
+		memcpy(addrs[i], addr_1, 19);
+		printf("%d) %s  %s\n", i,  addr_1, name);
 
-      write(s, &command, 1);
-
-
-       ThinkGearStreamParser parser;
-     THINKGEAR_initParser( &parser, PARSER_TYPE_PACKETS,
-                          handleDataValueFunc, NULL );
-
-         while( bytes_read = read(s, &buf, sizeof(buf)) ) {
-
-
-               THINKGEAR_parseByte( &parser, buf);
-
-
-         }
-
-
-
-
-
-     }
+ 	}
 
 
-    close(s);
-    return 0;
+	printf("Select device: ");
+	scanf("%d",&pos);
+	if( pos >= num_rsp || pos < 0){
+		printf("not available\n");
+		return 0;
+	}
+
+	printf("%s selected\n",addrs[pos]);
+	strcpy(dest,addrs[pos]);
+	free( ii );
+	for(i = 0; i < num_rsp; i++)
+		free(addrs[i]);
+        free(addrs);
+        close( sock );
+
+	// allocate a socket
+    	s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    	// set the connection parameters (who to connect to)
+    	addr.rc_family = AF_BLUETOOTH;
+    	addr.rc_channel = (uint8_t) 1;
+    	str2ba( dest, &addr.rc_bdaddr );
+
+    	// connect to server
+    	status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+        if( status != 0 ) perror("eu Ruim");
+    	else{
+		write(s, &command, 1);
+
+
+       		ThinkGearStreamParser parser;
+     		THINKGEAR_initParser( &parser, PARSER_TYPE_PACKETS,handleDataValueFunc, NULL );
+        	while( bytes_read = read(s, &buf, sizeof(buf)) ) {
+               		THINKGEAR_parseByte( &parser, buf);
+         	}
+     	}
+
+    	close(s);
+    	return 0;
 }
